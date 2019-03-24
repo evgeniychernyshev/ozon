17a1590709ab6d193e26e451d8795fe2985463f3
student <student@local.ru>
2019-03-24 12:27:06 +0300

    New pages and funcs

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for
+from werkzeug.utils import redirect
 
-from app.ozon import create_book, add_book, search_book_by_title
+from app.ozon import create_book, add_book, search_book_by_title, search_book_by_id, remove_book_by_id, \
+    create_empty_book
 
 
 def main():
@@ -24,7 +26,41 @@ def main():
 
         return render_template("index.html", books=container)
 
+    @app.route('/books/<book_id>')
+    def book_details(book_id):
+        result = search_book_by_id(container, book_id)
+        return render_template('book-details.html', book=result)
+
+    @app.route('/books/<book_id>/edit')
+    def book_edit(book_id):
+        book = None
+        if book_id == 'new':
+            book = create_empty_book()
+        else:
+            book = search_book_by_id(container, book_id)
+        return render_template('book-edit.html', book=book)
+
+    @app.route('/books/<book_id>/save', methods=['POST'])
+    def book_save(book_id):
+        nonlocal container
+        title = request.form['title']
+        author = request.form['author']
+        if book_id == 'new':
+            book = create_book(title=title, author=author)
+            container = add_book(container, book)
+        else:
+            book = search_book_by_id(container, book_id)
+            pass # TODO сохранить изменеия
+        return redirect(url_for('book_details', book_id=book['id']))
+
+    @app.route('/books/<book_id>/remove', methods=['POST'])
+    def book_remove(book_id):
+        nonlocal container
+        container = remove_book_by_id(container, book_id)
+        return redirect(url_for('index'))
+
     app.run(port=9876, debug=True)
+
 
 
 
--- app/ozon.py
+++ app/ozon.py
@@ -1,9 +1,18 @@
+import uuid
+
 def create_book(title, author):
     return {
+        'id': str(uuid.uuid4()),
         'title': title,
         'author': author,
     }
 
+def create_empty_book():
+    return {
+        'id': str(uuid.UUID()),
+        'title': '',
+        'author': '',
+    }
 
 def add_book(container, book):
     copy = container[:]
@@ -17,5 +26,18 @@ def search_book_by_title(container, title):
         if book['title'] == title:
             result.append(book)
         return result
+
+def search_book_by_id(container, id):
+    for book in container:
+        if book['id'] == id:
+            return book
+
+def remove_book_by_id(container, book_id):
+    result = []
+    for book in container:
+        if book['id'] != book_id:
+            result.append(book)
+    return result
+
 
 
